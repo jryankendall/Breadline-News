5a8a4eeb567e8f3ff905df343c95f4b31efbc5f0
unknown <jed@jedkendall.com>
2019-05-21 15:15:34 -0500

    Added base handlebars code. Basic route added.

--- assets/routes/htmlRoutes.js
+++ assets/routes/htmlRoutes.js
@@ -1,5 +1,9 @@
 //This file contains my routes created that will serve the handlebars pages
 
 module.exports = function(app) {
-    //Code in here
+    app.get("/", (req, res) => {
+        console.log(req);
+        
+        
+    })
 }
--- server.js
+++ server.js
@@ -1,6 +1,39 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("./assets/public"));
+
+// Handlebars
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
+);
+app.set("view engine", "handlebars");
+
+//Routes
+/* require("./assets/routes/apiRoutes")(app);
+require("./assets/routes/htmlRoutes")(app); */
+
+
+//starting the server
+app.listen(PORT, function() {
+    console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+    );
+});
